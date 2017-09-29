50dc5646c9c91d2aa233c06c8e2653ff1ea181b1
nkalambur <kalambur@umich.edu>
2017-09-29 09:48:13 -0400

    second push

--- SI507F17_project1_cards.py
+++ SI507F17_project1_cards.py
@@ -34,11 +34,7 @@ class Deck(object):
                 self.cards.append(card) # appends in a sorted order
 
     def __str__(self):
-        total = []
-        for card in self.cards:
-            total.append(card.__str__())
-        # shows up in whatever order the cards are in
-        return "\n".join(total) # returns a multi-line string listing each card
+        return "{rank} of {suit}".format({"rank": self.rank, "suit": self.suit })
 
     def pop_card(self, i=-1):
         return self.cards.pop(i) # this card is no longer in the deck -- taken off
